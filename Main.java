bb02c9186e0708aecf22dbdb497aca4e69de90f6
BielasR <126687810+BielasR@users.noreply.github.com>
2023-04-26 12:48:14 +0000

    nazwisko

--- Main.java
+++ Main.java
@@ -1,32 +1,44 @@
-/*
-Kod bazowy programu Commit4_0: 
-• Program dodaje do prostej bazy danych (pliku db.txt) dane odnośnie Studentów.
-• Studenci dodawani są w klasie Main.
-• Wszyscy studenci są wypisywani na końcu klasy Main.
-• Klasa Service obsługuje odczyt i zapis do pliku bazy danych.
-• Klasa Student reprezentuje pojedynczego studenta (Imię, Wiek).
-*/
 import java.util.Scanner;
 import java.io.IOException;
 
 class Main 
 {
   public static void main(String[] args) 
-  {
+ {
+    Service s = new Service();
+    Scanner wyb = new Scanner(System.in);
     Scanner imie = new Scanner(System.in);
     Scanner wiek = new Scanner(System.in);
     Scanner data = new Scanner(System.in);
-    System.out.println("Podaj imię: ");
-    String i = imie.nextLine();
-    System.out.println("Podaj wiek: ");
-    int w = wiek.nextInt();
-    System.out.println("Podaj datę urodzenia: ");
-    String d = data.nextLine();
-    try {
-      Service s = new Service();
+    System.out.println("Wybierz opcje\n 0.Zamknij program.\n 1.Dodaj nowego studenta.\n 2.Wypisz studentów");
+    int ww = wyb.nextInt();
+      switch(ww)
+    {
+      case 0:
+        System.exit(0);
+        break;
+      case 1:   
+        System.out.println("Podaj nowego studenta: ");
+        String i =imie.nextLine();
+        System.out.println("Podaj wiek studenta: ");
+        int w = wiek.nextInt();
+        System.out.println("Podaj datę urodzenia: ");
+        String d = data.nextLine();
+          try {
+     
       s.addStudent(new Student(i, w, d));
-   
+      
 
+      var students = s.getStudents();
+      for(Student current : students) {
+        
+      }
+    } catch (IOException e) {
+
+    }
+        break;
+     case 2:
+     try {
       var students = s.getStudents();
       for(Student current : students) {
         System.out.println(current.ToString());
@@ -35,4 +47,5 @@ class Main
 
     }
   }
+ }
 }
